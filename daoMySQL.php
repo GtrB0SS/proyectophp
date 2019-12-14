558e681ea84bba3504712d100d87adc592c105fc
Carlos <c.hdezbueno@gmail.com>
2019-12-14 11:18:47 +0100

    a

--- daoMySQL.php
+++ daoMySQL.php
@@ -14,8 +14,46 @@ function getConnection(){
 }
 
 
-function login(){
-    
+function login($user, $pwd){
+    $conex = getConnection();
+
+
+    $query="SELECT dni, clave
+            FROM cliente 
+            WHERE (dni LIKE '$user') and 
+                (clave LIKE '$pwd')";
+    $res_valid=mysqli_query($conex,$query) 
+            or die (mysqli_error($conex));
+    if ((mysqli_num_rows ($res_valid)==0) || !$user || !$pwd)
+    {
+        $query="SELECT numeroempleado, clave, especialidad
+        FROM empleado 
+        WHERE (numeroempleado LIKE '$user') and 
+            (clave LIKE '$pwd')";
+        $res_valid=mysqli_query($conex,$query) 
+                or die (mysqli_error($conex));
+        if ((mysqli_num_rows ($res_valid)==0) || !$user || !$pwd)
+        {
+            print_r("clave o usuario erroneo");
+
+        }
+        else
+        {
+            $reg_usuario=mysqli_fetch_array($res_valid);
+            $_SESSION['numeroempleado'] = $reg_usuario['numeroempleado'];
+            $_SESSION['especialidad'] = $reg_usuario['especialidad'];
+            return;
+
+        }
+
+    }
+    else
+    {
+        $reg_usuario=mysqli_fetch_array($res_valid);
+        $_SESSION['dni'] = $reg_usuario['dni'];
+        return;
+
+    }
     
     
 }
