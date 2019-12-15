aaa3fdffaa116c30448b429332a8c9f85c75ed95
Carlos <c.hdezbueno@gmail.com>
2019-12-15 18:14:54 +0100

    cosas

--- formNuevoEjercicio.php
+++ formNuevoEjercicio.php
@@ -7,22 +7,116 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Motofitness</title>
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     </head>
-    <body>
+    <body class="container">
         <?php
-        session_start();
-        include "daoMySQL.php";
-        
+        // put your code here
         ?>
-        <form action="insertEjercicio.php" action="POST">
-            <p>Nombre: <input type="text" name="nombreEj"/></p>
-            <p>Series: <input type="number" name="seriesEj"/></p>
-            <p>Repeticiones: <input type="number" name="repeticionesEj"/></p>
-            <p>Peso: <input type="number" name="pesoEj"/></p>
-            <p>Link: <input type="text" name="linkEj"/></p>
-            <p><input type="submit"/></p>
+        <nav class='navbar navbar-light bg-light row'>
+            <?php
+            // put your code here
+            session_start();
+            include "../daoMySQL.php";
+
+
+            //$_SESSION['dni'] = $_POST['user'];
+
+            if ($_SESSION['resLogin'] == "cliente" || $_SESSION['resLogin'] == "empleado") {
+
+                $plan = getPlan($_SESSION['dni']);
+
+                if ($plan != null && $plan = "pro") {
+
+                    $linktabla = "../tablas.php";
+                } else {
+                    $linktabla = "../ampliarplan.php";
+                }
+
+                print(" 
+                            <a class='navbar-brand' href='../index.php'>Inicio</a>
+                            <a class='navbar-brand' href='../progreso.php'>Progreso</a>
+                            <a class='navbar-brand' href='../dietas.php'>Dietas</a>
+                            <a class='navbar-brand' href='$linktabla'>Tabla de ejercicios</a>
+                        ");
+                if ($_SESSION['resLogin'] == "empleado") {
+
+                    print("<a class='navbar-brand' href='../admin.php'>Administracion</a>");
+                }
+                print("<a class='navbar-brand' href='../logout.php'>Logout</a>");
+            }
+            ?>
+        </nav>
+
+        <h3>Nuevo Ejercicio</h3>
+        <form action="gestiontablas.php" method="post">
+            <label>Nombre del ejercicio:</label>
+            <div class="form-group">
+                <input type="text" class="form-control" name="nombre" placeholder="Nombre del ejercicio" value="<?php
+                if (isset($_SESSION['insertejercicio']['nombre'])) {
+                    print($_SESSION['insertejercicio']['nombre']);
+                }
+                ?>" /><?php
+                       if (isset($_SESSION['erroresejercicio']['nombre'])) {
+                           print($_SESSION['erroresejercicio']['nombre']);
+                       }
+                       ?>
+            </div>
+
+            <label>Series:</label>
+            <div class="form-group">
+                <input type="text" class="form-control" name="series" placeholder="Series del ejercicio" value="<?php
+                if (isset($_SESSION['insertejercicio']['series'])) {
+                    print($_SESSION['insertejercicio']['series']);
+                }
+                ?>" /><?php
+                       if (isset($_SESSION['erroresejercicio']['series'])) {
+                           print($_SESSION['erroresejercicio']['series']);
+                       }
+                       ?>
+            </div>
+
+            <label>Repeticiones:</label>
+            <div class="form-group">
+                <input type="text" class="form-control" name="repeticiones" placeholder="Repeticiones del ejercicio" value="<?php
+                if (isset($_SESSION['insertejercicio']['repeticiones'])) {
+                    print($_SESSION['insertejercicio']['repeticiones']);
+                }
+                ?>" /><?php
+                       if (isset($_SESSION['erroresejercicio']['repeticiones'])) {
+                           print($_SESSION['erroresejercicio']['repeticiones']);
+                       }
+                       ?>
+            </div>
+
+            <label>Link del ejercicio:</label>
+            <div class="form-group">
+                <input type="text" class="form-control" name="link" placeholder="Link del ejercicio" value="<?php
+                if (isset($_SESSION['insertejercicio']['link'])) {
+                    print($_SESSION['insertejercicio']['link']);
+                }
+                ?>" /><?php
+                       if (isset($_SESSION['erroresejercicio']['link'])) {
+                           print($_SESSION['erroresejercicio']['link']);
+                       }
+                       ?>
+            </div>
+
+            <br>
+            <div class="form-group">
+                <input type="submit" class="btnSubmit" value="Insertar ejercicio" />
+            </div>
+
+            <input type="hidden" class="form-control" name="option"  value="ejercicio" />
         </form>
-        
+
+        <?php
+        if (isset($_SESSION['erroresejercicio'])) {
+            unset($_SESSION['erroresejercicio']);
+        }
+        ?>
     </body>
 </html>
